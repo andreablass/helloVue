80f1e9fbd6f1df72d6aca53bc5f341610e6928c8
andreablass <andreablass11@gmail.com>
2025-06-03 09:56:16 -0600

    WIP
      - Vue router adding, feed and profile view

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -3,5 +3,14 @@ const props = defineProps({})
 </script>
 
 <template>
-    <div>Mi perfil</div>
+<h1>Perfil</h1>
+  <div class="bg-white rounded shadow p-4 max-w-md mx-auto mt-6 flex items-center">
+    <div>
+      <h2 class="text-xl font-bold">Andrea Blass</h2>
+      <p class="text-gray-600 text-sm">From morelos🌞</p>
+      <button class="mt-2 px-3 py-1 bg-pink-500 text-white rounded text-sm">
+        Editar perfil
+      </button>
+    </div>
+  </div>
 </template>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,12 @@
 import Home from "@/pages/Home.vue";
 import Profile from "@/pages/Profile.vue";
 import NotFound from "@/pages/NotFound.vue";
+import Practice from "@/pages/Practice.vue"
 
 export default[
     { path: '/', component: Home, name: 'home' },
     { path: '/my-profile', component: Profile, name: 'my-profile' },
+    { path: '/practice', component: Practice, name: 'practice' },
     { path: '/profile/:username', component: Profile, name: 'profile' },
     { path: '/:pathMatch(.*)*', component: NotFound, name: 'not-found' },
 ];
